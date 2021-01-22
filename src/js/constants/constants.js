1ce0d3795416c2da8b3c41c748725cf6390c159a
Guseva Anastasia <kaa16080709@yandex.ru>
2021-01-22 10:16:20 +0300

    Updates

--- src/js/constants/constants.js
+++
@@ -1,8 +0,0 @@
-const townUrl =
-  NODE_ENV === 'production' ? 'https://kladr-api.ru' : 'https://kladr-api.ru';
-
-export const configTown = {
-  url: `${townUrl}/api.php?`,
-  type: 'city',
-  limit: 10,
-};
